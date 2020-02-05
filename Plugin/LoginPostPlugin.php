2e713a9b72fad8595e4be9aad882aba4d61052fe
Ilche Bedelovski <ilche.bedelovski@gmail.com>
2020-02-05 21:34:18 +0100

    Initial commit

---
+++ Plugin/LoginPostPlugin.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Onlymeat\Core\Plugin;
+
+class LoginPostPlugin {
+
+    /**
+     * Change redirect after login to home instead of dashboard.
+     *
+     * @param \Magento\Customer\Controller\Account\LoginPost $subject
+     * @param \Magento\Framework\Controller\Result\Redirect $result
+     */
+    public function afterExecute(
+        \Magento\Customer\Controller\Account\LoginPost $subject,
+        $result)
+    {
+        $result->setPath('products');
+        return $result;
+    }
+
+}
